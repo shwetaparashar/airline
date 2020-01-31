65732f2ab210027a301484be7e4a8af2f775f2b7
shwetaparashar <shweta.parashar@stcl.com>
2020-01-31 13:40:11 +0530

    Changes Testing Commit

--- flights/models.py
+++ flights/models.py
@@ -26,3 +26,13 @@ class Passenger(models.Model):
 
     def __str__(self):
         return f"{self.first} {self.last}"
+
+
+
+class Passenger(models.Model):
+        first = models.CharField(max_length=64)
+        last = models.CharField(max_length=64)
+        flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+        def __str__(self):
+            return f"{self.first} {self.last}"
